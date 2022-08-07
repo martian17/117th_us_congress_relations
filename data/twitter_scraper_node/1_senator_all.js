07e14aee7643f9c4f0b1a0d0f76ff3697c9cafad
martian17 <yakumodanshi@gmail.com>
2022-08-07 14:53:21 +0900

    debloat with get_all_data util function

--- data/twitter_scraper_node/1_senator_all.js
+++ data/twitter_scraper_node/1_senator_all.js
@@ -2,41 +2,29 @@
 //results in 1_senator_all.json
 
 let fs = require("fs");
-let {request_async,get_response_body} = require("./utils/request.js");
-let {API_key, API_key_secret, Bearer_Token} = JSON.parse(fs.readFileSync("./tokens.json"));
+let {stderr} = require("./utils/util.js");
+let {get_all_data} = require("./utils/twitter_api.js");
 
 
 let main = async function(){
-    //api link
+    stderr.log("Requesting data from the US Senate list on twitter");
+    stderr.log("Page URL: https://twitter.com/i/lists/63915645?lang=en");
+    stderr.log("API URL: https://api.twitter.com/2/lists/63915645/members");
+    //api docs link
     //https://developer.twitter.com/en/docs/api-reference-index
-    let pagination_token = null;
-    let result = [];
-    while(true){
-        let response = await request_async({
-            hostname:"api.twitter.com",
-            port:443,
-            path:`/2/lists/63915645/members${pagination_token?`?pagination_token=${pagination_token}`:""}`,
-            //63915645 is the id of US senate list
-            //page link: https://twitter.com/i/lists/63915645?lang=en
-            method:"GET",
-            headers:{
-                "Authorization": `Bearer ${Bearer_Token}`
-            }
-        });
-        let r = JSON.parse(await get_response_body(response));
-        //append the data
-        for(let entry of r.data){
-            result.push(entry);
-        }
-        if("next_token" in r.meta){
-            pagination_token = r.meta.next_token;
-        }else{
-            break;
-        }
-    }
-    console.log("[\n"+result.map((s)=>{
-        return "    "+JSON.stringify(s);
-    }).join(",\n")+"\n]");
+    let result = await get_all_data(`/2/lists/63915645/members`);
+    //63915645 is the id of US senate list
+    //page link: https://twitter.com/i/lists/63915645?lang=en
+    stderr.log(`Execution complete, got ${result.length} senator account metadata entries`);
+    fs.writeFileSync(
+        "./1_senator_all.json",
+        //beautify the formatting
+        "[\n"+result.map((s)=>{
+            return "    "+JSON.stringify(s);
+        }).join(",\n")+"\n]\n",
+        "utf-8"
+    );
+    stderr.log("Written the result to 1_senator_all.json");
 };
 
 main();
