d642909fa83d4cf7c8af001655cd90847a9a078a
martian17 <yakumodanshi@gmail.com>
2022-08-07 09:32:21 +0900

    replace process.stderr.write with stderr.log

--- data/twitter_scraper_node/3_cleanup.js
+++ data/twitter_scraper_node/3_cleanup.js
@@ -1,7 +1,7 @@
 // execute as `node 3_cleanup.js > 3_senators_mapped.json`
 
 let fs = require("fs");
-
+let {stderr} = require("./util.js");
 let {sen_all,sen_100} = JSON.parse(fs.readFileSync("./2_intersection_corrected.json"));
 
 
@@ -13,7 +13,7 @@ for(let sen of sen_100){
 //map the account to sen_100
 for(let account of sen_all){
     if(account.owners.length !== 1){
-        process.stderr.write(`irregular account: ${JSON.stringify(account,null,4)}\n`);
+        stderr.write(`irregular account: ${JSON.stringify(account,null,4)}\n`);
         continue;
     }
     let owner = account.owners[0];
